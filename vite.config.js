b575306c8f2807a3e89eafd4b2c7aac71b7f45bc
Jatin <jy799914@gmail.com>
2026-03-18 23:17:51 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
+  plugins: [react(), 
     tailwindcss(),
   ],
-  base:"/Pro4Gallary/",
+  base:"/Gallary_photo",
 })
